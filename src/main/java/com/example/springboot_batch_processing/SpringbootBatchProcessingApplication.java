91e4f2d59100aac406ff45509efa846cbd45c50c
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-08-13 13:02:41 -0500

    Initial Set up with dependencies and properties

---
+++ src/main/java/com/example/springboot_batch_processing/SpringbootBatchProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_batch_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBatchProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_batch_processing/SpringbootBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_batch_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
